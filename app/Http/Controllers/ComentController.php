affb7dce91bd83e7985ebd80954dd9da67dee106
haifahattab <hattab_haifa@yahoo.com>
2021-02-18 11:45:06 +0100

    models

---
+++ app/Http/Controllers/ComentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Coment;
+use Illuminate\Http\Request;
+
+class ComentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Coment  $coment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Coment $coment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Coment  $coment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Coment $coment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Coment  $coment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Coment $coment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Coment  $coment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Coment $coment)
+    {
+        //
+    }
+}
